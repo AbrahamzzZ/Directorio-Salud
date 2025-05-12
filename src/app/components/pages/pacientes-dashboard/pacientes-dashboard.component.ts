d5b7b3aac05fb29d1d855c14f5e338e07072365b
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-05-12 00:27:56 -0500

    Se agrego los card para mostrar los profesionales junto con un buscador

--- src/app/components/pages/pacientes-dashboard/pacientes-dashboard.component.ts
+++ src/app/components/pages/pacientes-dashboard/pacientes-dashboard.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
-import { ServLoginService } from '../../../services/serv-login.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Profesional } from '../../../models/Profesional';
+import { ServProfesionalesService } from '../../../services/serv-profesionales.service';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-pacientes-dashboard',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MatCardModule, MatButtonModule, FormsModule, MatInputModule],
   templateUrl: './pacientes-dashboard.component.html',
   styleUrl: './pacientes-dashboard.component.css'
 })
 export class PacientesDashboardComponent {
+  public profesionales: Profesional[] = [];
+  public terminoBusqueda: string = '';
 
+  constructor(private servicio: ServProfesionalesService) {
+    this.cargarTodos();
+  }
+
+  cargarTodos(): void {
+    this.servicio.getProfesionales().subscribe(data => {
+      this.profesionales = data;
+    });
+  }
+
+  buscar(searchInput: HTMLInputElement) {
+    const termino = searchInput.value.trim();
+    if (termino) {
+      this.servicio.buscarProfesional(termino).subscribe(data => {
+        this.profesionales = data;
+      });
+    } else {
+      this.cargarTodos();
+    }
+  }
 }
