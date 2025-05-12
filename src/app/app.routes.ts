6ab82bcf39656c7bae190691eebb6e899476840e
Gabo-UG <gabolopezino@gmail.com>
2025-05-12 15:31:34 -0500

    Avance CRUD Ctas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,21 @@ import { ProfesionalDashboardComponent } from './components/pages/profesional-da
 import { MantenimientoServicioComponent } from './components/pages/mantenimiento-servicio/mantenimiento-servicio.component';
 import { PacientesDashboardComponent } from './components/pages/pacientes-dashboard/pacientes-dashboard.component';
 import { RegistroActualizacionServicioComponent } from './components/pages/registro-actualizacion-servicio/registro-actualizacion-servicio.component';
+import { ListaServiciosComponent } from './components/pages/lista-servicios/lista-servicios.component';
+import { MantenimientoPacienteCitaComponent } from './components/pages/mantenimiento-paciente-cita/mantenimiento-paciente-cita/mantenimiento-paciente-cita.component';
+import { RegistroActualizacionCitaComponent } from './components/pages/registro-actualizacion-cita/registro-actualizacion-cita/registro-actualizacion-cita.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'profesional-dashboard', component: ProfesionalDashboardComponent },
+    { path: 'pacientes-dashboard', component: PacientesDashboardComponent }, 
+    { path: 'registro-actualizacion-cita', component: RegistroActualizacionCitaComponent },
+    { path: 'mantenimiento-paciente-cita', component: MantenimientoPacienteCitaComponent }, 
     { path: 'my-services', component: MantenimientoServicioComponent },
     { path: 'service-register', component: RegistroActualizacionServicioComponent },
     { path: 'service-edit/:id', component: RegistroActualizacionServicioComponent },
     { path: 'patients-dashboard', component: PacientesDashboardComponent },
+    { path: 'lista-servicios', component: ListaServiciosComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '/login'}
 ];
---
+++ src/app/components/pages/mantenimiento-paciente-cita/mantenimiento-paciente-cita/mantenimiento-paciente-cita.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HeaderComponent } from "../../../shared/header/header.component";
+import { FooterComponent } from "../../../shared/footer/footer.component";
+
+
+@Component({
+  selector: 'app-mantenimiento-paciente-cita',
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './mantenimiento-paciente-cita.component.html',
+  styleUrl: './mantenimiento-paciente-cita.component.css'
+})
+export class MantenimientoPacienteCitaComponent {
+
+}
