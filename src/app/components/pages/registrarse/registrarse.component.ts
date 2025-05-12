703a70bf58373d40a1d63e7b4ddb43a5e0f10b2b
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-05-11 19:20:44 -0500

    Se creo la interfaz para elegir el tipo de usuario que es

---
+++ src/app/components/pages/registrarse/registrarse.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatSelectModule} from '@angular/material/select';
+
+@Component({
+  selector: 'app-registrarse',
+  imports: [MatSelectModule],
+  templateUrl: './registrarse.component.html',
+  styleUrl: './registrarse.component.css'
+})
+export class RegistrarseComponent {
+  constructor(private router: Router) {}
+
+  redirigir(tipo: string) {
+    if (tipo === 'profesional') {
+      this.router.navigate(['/profesional-register']);
+    } else if (tipo === 'paciente') {
+      this.router.navigate(['/registro-paciente']);
+    }
+  }
+}
