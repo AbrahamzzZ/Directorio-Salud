34183336ba2b4688eae11266a76e2a8c5feabd34
FishySLR <alejandrolarrea55@gmail.com>
2025-05-13 14:02:24 -0500

    Agregu rutas modulo paciente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { ProfesionalDashboardComponent } from './components/pages/profesional-da
 import { MantenimientoServicioComponent } from './components/pages/mantenimiento-servicio/mantenimiento-servicio.component';
 import { PacientesDashboardComponent } from './components/pages/pacientes-dashboard/pacientes-dashboard.component';
 import { RegistroActualizacionServicioComponent } from './components/pages/registro-actualizacion-servicio/registro-actualizacion-servicio.component';
+import { AdminDashboardComponent } from './components/pages/admin-dashboard/admin-dashboard.component';
+import { MantenimientoPacienteComponent } from './components/pages/mantenimiento-paciente/mantenimiento-paciente.component';
+import { RegistroActualizacionPacienteComponent } from './components/pages/registro-actualizacion-paciente/registro-actualizacion-paciente.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -12,6 +15,10 @@ export const routes: Routes = [
     { path: 'service-register', component: RegistroActualizacionServicioComponent },
     { path: 'service-edit/:id', component: RegistroActualizacionServicioComponent },
     { path: 'patients-dashboard', component: PacientesDashboardComponent },
+    { path: 'admin-dashboard', component: AdminDashboardComponent },//y
+    { path: 'mis-pacientes', component: MantenimientoPacienteComponent},//y
+    { path: 'pacientes-registro', component: RegistroActualizacionPacienteComponent},//y
+    { path: 'editar-pacientes/:id', component: RegistroActualizacionPacienteComponent},//y
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '/login'}
 ];
