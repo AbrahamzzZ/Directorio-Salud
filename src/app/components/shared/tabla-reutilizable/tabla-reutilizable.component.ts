4efa4fe6c4e27696aa3602a392b5109a71cc22b2
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-05-13 07:11:12 -0500

    Se creo un nueva tabla reutilizable

---
+++ src/app/components/shared/tabla-reutilizable/tabla-reutilizable.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, EventEmitter, Input, Output, signal, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+@Component({
+  selector: 'app-tabla-reutilizable',
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatIconModule, MatButtonModule],
+  templateUrl: './tabla-reutilizable.component.html',
+  styleUrl: './tabla-reutilizable.component.css'
+})
+export class TablaReutilizableComponent<T> implements AfterViewInit {
+  @Input() columnas: string[] = [];
+
+  private _data: T[] = [];
+  @Input() set data(value: T[]) {
+    this._data = value;
+    this.dataSource.data = value;
+  }
+
+  @Input() acciones: { tipo: string; icono: string; tooltip: string }[] = [];
+
+  @Output() accion = new EventEmitter<{ tipo: string, fila: T }>();
+
+  columnasSignal = signal(this.columnas);
+  dataSource = new MatTableDataSource<T>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  getTituloCabecera(col: string): string {
+    switch (col) {
+      case 'fechaDisponible':
+        return 'Fecha Disponible';
+      case 'requiereChequeo':
+        return 'Requiere Chequeo';
+      default:
+        return col.charAt(0).toUpperCase() + col.slice(1); // Capitaliza
+    }
+  }
+}
