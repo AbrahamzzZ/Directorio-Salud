449315977d09b1065903710a484e03fbc8fc350b
Angelvigar <angelvivanco223@gmail.com>
2025-05-12 22:15:53 -0500

    Creación de componente y servicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,22 @@ import { ProfesionalDashboardComponent } from './components/pages/profesional-da
 import { MantenimientoServicioComponent } from './components/pages/mantenimiento-servicio/mantenimiento-servicio.component';
 import { PacientesDashboardComponent } from './components/pages/pacientes-dashboard/pacientes-dashboard.component';
 import { RegistroActualizacionServicioComponent } from './components/pages/registro-actualizacion-servicio/registro-actualizacion-servicio.component';
+import { AdministradorDashboardComponent } from './components/pages/administrador-dashboard/administrador-dashboard.component';
+import { MantenimientoProfesionalComponent } from './components/pages/profesional/mantenimiento-profesional/mantenimiento-profesional.component';
+import { RegistroActualizacionProfesionalComponent } from './components/pages/profesional/registro-actualizacion-profesional/registro-actualizacion-profesional.component';
+import { RegistrarseComponent } from './components/pages/registrarse/registrarse.component';
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'registrar', component: RegistrarseComponent},
+    { path: 'profesional-dashboard', component: ProfesionalDashboardComponent, title: 'Profesional' },
+    { path: 'profesional-register', component:  RegistroActualizacionProfesionalComponent, title: 'Formulario de registro'},
+    { path: 'profesional-edit/:id', component: RegistroActualizacionProfesionalComponent, title: 'Formulario de edición'},
+    { path: 'list-profesional', component: MantenimientoProfesionalComponent, title: 'Listado de profesionales'},
     { path: 'my-services', component: MantenimientoServicioComponent },
     { path: 'service-register', component: RegistroActualizacionServicioComponent },
     { path: 'service-edit/:id', component: RegistroActualizacionServicioComponent },
     { path: 'patients-dashboard', component: PacientesDashboardComponent },
-
+    { path: 'admin-dashboard', component: AdministradorDashboardComponent, title: 'Administrador'},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '/login'}
 ];
