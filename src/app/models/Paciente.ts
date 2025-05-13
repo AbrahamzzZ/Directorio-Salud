9725571357926c8968701728394407a39133efc1
FishySLR <alejandrolarrea55@gmail.com>
2025-05-13 14:09:14 -0500

    Modelo Paciente

---
+++ src/app/models/Paciente.ts
@@ -0,0 +1,12 @@
+export interface Paciente {    
+      id: string,
+      nombre: string,
+      correo: string,
+      telefono: string,
+      Edad: number,
+      diagnostico: string,
+      contacto: string,
+      tipoSangre: string,
+      fechaRegistro: string,    
+      estado: Boolean
+}
