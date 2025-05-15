183a949f2066a7bf6f03b85d2475923e5260c6d9
FishySLR <alejandrolarrea55@gmail.com>
2025-05-15 00:25:23 -0500

    cree la carpeta paciente para el componente

---
+++ src/app/components/pages/paciente/mantenimiento-paciente/mantenimiento-paciente.component.ts
@@ -0,0 +1,109 @@
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { HeaderComponent } from '../../../shared/header/header.component';
+import { FooterComponent } from '../../../shared/footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Paciente } from '../../../../models/Paciente';
+import { ServPacientesService } from '../../../../services/servicio-paciente/serv-pacientes.service';
+import { Router } from '@angular/router';
+import { TablaReutilizableComponent } from '../../../shared/tabla-reutilizable/tabla-reutilizable.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
+@Component({
+  selector: 'app-mantenimiento-paciente',
+  standalone: true,
+  imports: [
+    HeaderComponent, 
+    FooterComponent, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatFormFieldModule,
+    TablaReutilizableComponent, 
+    MatIconModule,
+    MatTableModule,
+    MatPaginatorModule
+  ],
+  templateUrl: './mantenimiento-paciente.component.html',
+  styleUrl: './mantenimiento-paciente.component.css'
+})
+export class MantenimientoPacienteComponent implements OnInit, AfterViewInit {
+  constructor(private servicePacientes: ServPacientesService, private router: Router) {}
+
+  dataSource = new MatTableDataSource<Paciente>();
+  col: string[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  columns = [
+    { key: 'nombre', titulo: 'Nombre' },
+    { key: 'correo', titulo: 'Correo' },
+    { key: 'telefono', titulo: 'Telefono' },
+    { key: 'Edad', titulo: 'Edad' }, 
+    { key: 'diagnostico', titulo: 'Diagnostico' },
+    { key: 'tipoSangre', titulo: 'Tipo de Sangre' },
+    { key: 'fechaRegistro', titulo: 'Fecha de Registro' }, 
+    { key: 'estado', titulo: 'Estado'}       
+  ];
+
+  displayedColumns: string[] = [...this.columns.map(cm => cm.key), 'Acciones'];
+
+  acciones = [
+    { tipo: 'editar', icono: 'edit', tooltip: 'Editar paciente' },
+    { tipo: 'eliminar', icono: 'delete', tooltip: 'Eliminar paciente' }
+  ];
+
+  ngOnInit(): void {
+    this.col = this.columns.map(cm => cm.key);
+    this.displayedColumns = [...this.col, 'Acciones'];
+    this.cargarPacientes();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  cargarPacientes(): void {
+    this.servicePacientes.getpacientes().subscribe((datos: Paciente[]) => {
+      this.dataSource.data = datos;
+    });
+  }
+
+  search(searchInput: HTMLInputElement) {
+    const texto = searchInput.value.trim();
+    if (texto) {
+      this.servicePacientes.getpacientesSearch(texto).subscribe((datos: Paciente[]) => {
+        this.dataSource.data = datos;
+      });
+    } else {
+      this.cargarPacientes();
+    }
+  }
+
+  manejarAccion(evento: { tipo: string, fila: Paciente }) {
+    switch (evento.tipo) {
+      case 'editar':
+        this.editar(evento.fila);
+        break;
+      case 'eliminar':
+        this.eliminar(evento.fila);
+        break;
+    }
+  }
+
+  editar(paciente: Paciente) {
+    this.router.navigate(['editar-pacientes', paciente.id]);
+  }
+
+  eliminar(paciente: Paciente) {
+    const confirmar = confirm("¿Estas seguro de eliminar al paciente " + paciente.nombre + "?");
+    if (confirmar) {
+      this.servicePacientes.deletePatient(paciente).subscribe(() => {
+        alert("Paciente eliminado exitosamente");
+        this.cargarPacientes();
+      });
+    }
+  }
+}
---
+++ src/app/components/pages/paciente/registro-actualizacion-paciente/registro-actualizacion-paciente.component.ts
@@ -0,0 +1,124 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ServPacientesService } from '../../../../services/servicio-paciente/serv-pacientes.service';
+import { Paciente } from '../../../../models/Paciente';
+import { HeaderComponent } from "../../../shared/header/header.component";
+import { FooterComponent } from "../../../shared/footer/footer.component";
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-registro-actualizacion-paciente',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, MatInputModule, MatFormFieldModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule,
+  MatButtonModule, MatRadioModule, MatCheckboxModule, MatCardModule, HeaderComponent, FooterComponent, MatIconModule],
+  templateUrl: './registro-actualizacion-paciente.component.html',
+  styleUrl: './registro-actualizacion-paciente.component.css'
+})
+export class RegistroActualizacionPacienteComponent implements OnInit {
+  fechaMin: Date = new Date();
+  pacienteForm: FormGroup;
+  isEditMode = false;
+  pacienteId: string | null = null;
+  pacienteOriginal: Paciente | null = null;
+  tiposDeSangre: string[] = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+  metodosContacto: string[] = ['email', 'llamada telefónica', 'SMS'];
+
+  constructor(
+    private fb: FormBuilder,
+    private pacientesService: ServPacientesService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.pacienteForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3), Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$')]],
+      correo: ['', [Validators.required, Validators.email]],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      Edad: ['', [Validators.required, Validators.min(0), Validators.max(120)]],
+      diagnostico: ['', [Validators.required, Validators.minLength(5)]],
+      contacto: ['', Validators.required],
+      tipoSangre: ['', Validators.required],
+      fechaRegistro: ['', Validators.required],
+      estado: [true]
+    });
+  }
+
+  ngOnInit(): void {    
+    this.pacienteId = this.route.snapshot.paramMap.get('id');
+    if (this.pacienteId) {
+      this.isEditMode = true;
+      this.pacientesService.getpacientes().subscribe(pacientes => {
+        const paciente = pacientes.find(p => p.id === this.pacienteId);
+        if (paciente) {
+          this.pacienteOriginal = paciente;
+          this.populateForm(paciente);
+        } else {        
+          alert('El paciente no se encuentra registrado');
+          this.router.navigate(['/admin-dashboard']);
+        }
+      });
+    }
+  }
+
+  populateForm(paciente: Paciente): void {
+    this.pacienteForm.patchValue({
+      nombre: paciente.nombre,
+      correo: paciente.correo,
+      telefono: paciente.telefono,
+      Edad: paciente.Edad,
+      diagnostico: paciente.diagnostico,
+      contacto: paciente.contacto,
+      tipoSangre: paciente.tipoSangre,
+      fechaRegistro: paciente.fechaRegistro,
+      estado: paciente.estado
+    });
+  }
+
+  onSubmit(): void {  
+    if (this.isEditMode) {
+      this.updatePaciente();
+    } else {
+      this.addPaciente();
+    }
+  }
+
+  addPaciente(): void {    
+    const nuevo = 'p' + Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+    
+    const newPaciente: Paciente = {
+      id: nuevo,
+      ...this.pacienteForm.value
+    };
+
+    this.pacientesService.addPatient(newPaciente).subscribe(() => {
+      alert("Paciente registrado exitosamente");
+      this.router.navigate(['/mis-pacientes'], { replaceUrl: true });
+    });
+  }
+
+  updatePaciente(): void {
+    const updatedPaciente: Paciente = {
+      ...this.pacienteOriginal!,
+      ...this.pacienteForm.value
+    };    
+    this.pacientesService.editPatient(updatedPaciente).subscribe(() => {
+      alert("El paciente se ha actualizado correctamente");
+      this.router.navigate(['/mis-pacientes'], { replaceUrl: true });
+    });
+  } 
+
+  onCancel(): void {
+    this.router.navigate(['/mis-pacientes']);
+  }
+  
+}
