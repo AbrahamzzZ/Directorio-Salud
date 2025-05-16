4c5a0506132c7ff3859e06ba8f0d1973dc3a1e33
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-05-16 00:43:34 -0500

    Se creo un guard para la autenticacion

--- src/app/guards/autenticacion.guard.ts
+++ src/app/guards/autenticacion.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ServLoginService } from '../services/serv-login.service';
 
-export const autenticacionGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+
+export class Autenticacion implements CanActivate {
+  constructor(private login: ServLoginService, private router:Router){}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const auth = this.login.estaAutenticado();
+
+    if (auth) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
